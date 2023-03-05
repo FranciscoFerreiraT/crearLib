3cdf08b78e1c587cc5cfe94a18ca50ce7c2eff44
FranciscoFerreiraT <92456485+FranciscoFerreiraT@users.noreply.github.com>
2023-03-05 20:49:54 +0100

    Add files via upload

---
+++ Calculadora/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"com.castelao","c":"Operaciones","l":"Operaciones()","u":"%3Cinit%3E()"},{"p":"com.castelao","c":"Operaciones","l":"operacionesBasicasConTryCatch(double, double)","u":"operacionesBasicasConTryCatch(double,double)"}];updateSearchResults();
---
+++ Calculadora/src/com/castelao/Operaciones.java
@@ -0,0 +1,22 @@
+package com.castelao;
+
+public class Operaciones {
+    public static void operacionesBasicasConTryCatch(double num1, double num2) {
+
+
+        try {
+            double suma = num1 + num2;
+            double resta = num1 - num2;
+            double multiplicacion = num1 * num2;
+            double division = num1 / num2;
+
+
+            System.out.println("Suma: " + suma);
+            System.out.println("Resta: " + resta);
+            System.out.println("Multiplicación: " + multiplicacion);
+            System.out.println("División: " + division);
+        } catch (ArithmeticException e) {
+            System.out.println("Error");
+        }
+    }
+}
