e4a4c992617a4f369b509503630f0bf131162cdf
FranciscoFerreiraT <92456485+FranciscoFerreiraT@users.noreply.github.com>
2023-03-05 21:25:21 +0100

    Ejercicio

--- Calculadora/src/com/castelao/Operaciones.java
+++ Calculadora/src/com/castelao/Operaciones.java
@@ -1,7 +1,7 @@
 package com.castelao;
 
 public class Operaciones {
-    public static void operaciones(double num1, double num2) {
+    public static void operacionesBasicasConTryCatch(double num1, double num2) {
 
 
         try {
@@ -9,18 +9,14 @@ public class Operaciones {
             double resta = num1 - num2;
             double multiplicacion = num1 * num2;
             double division = num1 / num2;
-            double raizCuadrada1 = Math.sqrt(num1);
-            double raizCuadrada2 = Math.sqrt(num2);
 
-            System.out.println("La suma de " + num1 + " y " + num2 + " es: " + suma);
-            System.out.println("La resta de " + num1 + " y " + num2 + " es: " + resta);
-            System.out.println("La multiplicación de " + num1 + " y " + num2 + " es: " + multiplicacion);
-            System.out.println("La división de " + num1 + " y " + num2 + " es: " + division);
-            System.out.println("La raíz cuadrada de " + num1 + " es: " + raizCuadrada1);
-            System.out.println("La raíz cuadrada de " + num2 + " es: " + raizCuadrada2);
 
+            System.out.println("Suma: " + suma);
+            System.out.println("Resta: " + resta);
+            System.out.println("Multiplicación: " + multiplicacion);
+            System.out.println("División: " + division);
         } catch (ArithmeticException e) {
-            System.out.println("Error: " + e.getMessage());
+            System.out.println("Error ");
         }
     }
-    }
+}
